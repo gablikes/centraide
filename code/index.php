3becd8af762e0cceaed2c8c81818d1fcc65d8592
Gabrielle Bernatchez <gbernatchez@evollia.com>
2013-07-17 15:53:14 -0400

    debut integration

--- code/index.php
+++ code/index.php
@@ -1,3 +1,134 @@
-<?php 
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width">
+	<title>Mon site</title>
+	<link href='http://fonts.googleapis.com/css?family=Enriqueta:400,700' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="text/css" href="style.css" />
+	<!--[if lt IE 9]>
+	<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
 
-echo 'yeah';
+
+</head>
+<body>
+
+<div id="page" class="container">
+
+	<aside id="sidebar" class="c2 first">
+
+		<section class="wrapper en-tete-aside">
+
+			<header>
+
+				<img width="90" alt="Logo de Centraide Québec et Chaudière-Appalache" src="/images/logo-centraide.png">
+				<h1>Votre campagne en ligne</h1>
+
+			</header>
+
+			<a class="bt-rose" href="#" title="Faire un don">Faire un don</a>
+
+		</section>
+
+		<section class="wrapper">
+
+			<h2>Connectez-vous</h2>
+
+		</section>
+
+		<section class="wrapper">
+
+			<h2>Progression de la campagne</h2>
+
+			<div id="progress">
+
+
+
+			</div>
+
+		</section>
+
+	</aside>
+
+	<div id="content" class="c5">
+
+		<h1>Dernière activité</h1>
+		
+
+	</div>
+
+</div>
+
+<div id="footer">
+
+	<div class="container">
+
+		<section class="c2 first">
+
+			<div class="wrapper aligncenter">
+
+				<a href="#" title="Centraide Québec"><img alt="Logo de Centraide Québec et Chaudière-Appalache" src="/images/logo-centraide.png"></a>
+
+			</div>
+
+		</section>
+
+		<div class="oh">
+
+			<section class="c1 c-tier">
+
+				<div class="wrapper">
+
+					<h3>Centraide</h3>
+
+					<ul>
+						<li><a href="#" title="Campagne Centraide">Campagne Centraide</a></li>
+						<li><a href="#" title="À propos">À propos</a></li>
+					</ul>
+
+					<a class="bt-rose" href="#" title="Faire un don">Faire un don</a>
+
+				</div>
+
+			</section>
+
+			<section class="c1 c-tier">
+
+				<div class="wrapper">
+
+					<h3>Restez informé</h3>
+
+					<form method='get' accept-charset='UTF-8' name='oi_form' action='http://suivi.lnk01.com/oi/1/313ec887ff3c03ff28d05a203e8eaec1'>
+						<label for="email" class="visuallyhidden">Courriel</label> 
+						<input type='email' name='email' placeholder="Votre courriel" />
+						<input type='hidden' name='goto' value='' />
+						<input type='hidden' name='iehack' value='&#9760;' />
+						<input class="bt-rose fr" type='submit' value='→' />
+					</form>
+
+				</div>
+
+			</section>
+
+			<section class="c1 c-tier">
+
+				<div class="wrapper">
+
+					<h3>Développeurs</h3>
+
+					<a href="#" title="Contribuer à cette application">Contribuer à cette application</a>
+
+				</div>
+
+			</section>
+
+		</div>
+
+	</div>
+
+</div>
+
+
+</body>
+</html>
